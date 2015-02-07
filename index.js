6973bb986cee51116292c73cc981fd4e42a1454b
Arjun <arjun.bali96@gmail.com>
2015-02-07 18:33:39 -0500

    separated into 2 apps and added annyang

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
+var trappedApp = require('express')();
+var trappedHTTP = require('http').Server(trappedApp);
+var trappedIO = require('socket.io')(trappedHTTP);
 var idNum = 0;
 
-io.on('connection', function(socket){
+var commanderApp = require('express')();
+var commanderHTTP = require('http').Server(commanderApp);
+var commanderIO = require('socket.io')(commanderHTTP);
+var orderArray = [];
+
+
+// trapped individuals
+
+trappedApp.get('/', function(req, res){
+  res.sendFile(__dirname + '/trapped.html');
+});
+
+trappedIO.on('connection', function(socket){
 
   idNum += 1;
-  io.emit('assign id', idNum);
+  trappedIO.emit('assign id', idNum);
   // idNum is assigned to the given client
   // on connection (simulate room numbers)
 
@@ -20,6 +27,26 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+trappedHTTP.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
+// commander
+
+commanderIO.on('connection', function(socket) {
+
+  socket.on('room order', function(roomNum) {
+    orderArray.push(rooomNum);
+  });
+
+});
+
+commanderApp.get('/', function(req, res){
+  res.sendFile(__dirname + '/commander.html');
+});
+
+commanderHTTP.listen(8080, function(){
+  console.log('listening on *:8080');
+});
+
